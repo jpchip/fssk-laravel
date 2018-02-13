bff9c4e66ebbc9c0df4ab1e4cc2c480d25b2166d
Jared Chapiewsky <chapiewsky@earthlinginteractive.com>
2018-02-13 13:32:13 -0600

    create symlink for production

--- deploy/server-entrypoint.sh
+++ deploy/server-entrypoint.sh
@@ -6,4 +6,7 @@ composer install -d=/var/www/server
 echo "Setting directory permissions";
 chown -R www-data.www-data /var/www && chmod 775 /var/www;
 
+echo "Create symlink for production builds";
+ln -s /var/www/client/build /var/www/server/public/client
+
 exec "$@"
